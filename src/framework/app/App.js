e01c4319189ca42074a4718a76d8a7ff088a76bc
Guylian Cox <guylian.cox@pictawall.com>
2016-11-14 14:26:50 +0100

    Server-side rendering in dev mode

--- src/framework/app/App.js
+++ src/framework/app/App.js
@@ -3,29 +3,24 @@
 // import 'file?name=[name].[ext]!./.htaccess';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { applyRouterMiddleware, Router } from 'react-router';
-import { useScroll } from 'react-router-scroll';
 import translationMessages from '../common/i18n';
-import { store, rootRoute, history } from '../common/kernel';
+import { store } from '../common/kernel';
 import LanguageComponent from './LanguageComponent';
 
-export default function App() {
+export default function App(props) {
 
   return (
     <Provider store={store}>
       <LanguageComponent messages={translationMessages}>
-        <Router
-          history={history}
-          routes={rootRoute}
-          render={
-            // Scroll to top when going to a new page, imitating default browser behaviour
-            applyRouterMiddleware(useScroll())
-          }
-        />
+        {props.children}
       </LanguageComponent>
     </Provider>
   );
 }
+
+App.propTypes = {
+  children: React.PropTypes.node,
+};
 
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
