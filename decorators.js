91043f65802f4258bfe0e9385e14227a1cbca4c1
Guylian Cox <guylian.cox@pictawall.com>
2016-11-25 15:26:00 +0100

    Fix route loading problems

--- decorators.js
+++ decorators.js
@@ -1,11 +1,2 @@
-/* eslint-disable */
-
-var getDefault = require('./lib/shared/util/ModuleUtil').getDefault;
-
-exports.container = getDefault(require('./lib/framework/common/decorators/container'));
-
-const ProviderNamespace = require('./lib/framework/common/decorators/provider');
-exports.provider = ProviderNamespace.provider;
-exports.reducer = ProviderNamespace.reducer;
-exports.saga = ProviderNamespace.saga;
-exports.action = ProviderNamespace.action;
+export { default as container } from './src/framework/common/decorators/container';
+export * from './src/framework/common/decorators/provider';
