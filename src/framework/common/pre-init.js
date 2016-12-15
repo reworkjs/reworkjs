a006f3fbb2292fe68914c4a0ca9252e2bb8ae452
Guylian Cox <guylian.cox@pictawall.com>
2016-12-15 16:00:23 +0100

    Print to debug whether pre-init was loaded or not

--- src/framework/common/pre-init.js
+++ src/framework/common/pre-init.js
@@ -1,14 +1,21 @@
 import loadPolyfills from '../common/load-polyfills';
 import { getDefault } from '../../shared/util/ModuleUtil';
+import logger from '../../shared/logger';
 
 export default async function loadPreInit() {
   await loadPolyfills();
 
   // webpack
   const preInit = getDefault(require('@@pre-init')); // eslint-disable-line
-  if (typeof preInit === 'function') {
-    await preInit();
+  if (!preInit) {
+    logger.debug('No pre-init file.');
   } else {
-    await preInit;
+    if (typeof preInit === 'function') {
+      await preInit();
+    } else {
+      await preInit;
+    }
+
+    logger.debug('pre-init loaded.');
   }
 }
