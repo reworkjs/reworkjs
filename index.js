d72f1b442d23a52dd25181956b41362781082e50
Guylian Cox <gc@pictawall.com>
2017-02-03 15:08:07 +0100

    Export store

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as logger } from './src/shared/logger';
 export { default as LanguageProvider } from './src/framework/app/providers/LanguageProvider';
+export { store } from './src/framework/common/kernel';
