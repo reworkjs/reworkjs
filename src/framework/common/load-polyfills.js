1155b59e50689ccb06a2a5b6a5b62c8765c6de56
Guylian Cox <gc@pictawall.com>
2017-03-15 16:47:56 +0100

    Use import instead of System.import

--- src/framework/common/load-polyfills.js
+++ src/framework/common/load-polyfills.js
@@ -1,18 +1,19 @@
 import global from 'global';
 import '../../shared/regenerator';
+import { isServer } from '../../shared/EnvUtil';
 
 export default function loadPolyfills() {
   const promises = [];
 
   if (!global.Intl) {
     promises.push(
-      System.import('intl'),
-      System.import('intl/locale-data/jsonp/en.js'),
+      import('intl'),
+      import('intl/locale-data/jsonp/en.js'),
     );
   }
 
-  if (webpack_globals.SIDE === 'server') { // eslint-disable-line
-    promises.push(System.import('source-map-support/register'));
+  if (isServer) { // eslint-disable-line
+    promises.push(import('source-map-support/register'));
   }
 
   return Promise.all(promises);
