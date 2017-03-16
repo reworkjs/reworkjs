416a1fe2e98fef0aa817f0ff2738658e322ef98b
Guylian Cox <gc@pictawall.com>
2017-03-16 10:21:00 +0100

    [start --env dev --no-prerendering]: Send index.html for all not found routes, let the client-side decide if it is a 404 or not.

--- src/internals/cli/build-webpack-client-dev.js
+++ src/internals/cli/build-webpack-client-dev.js
@@ -52,13 +52,13 @@ app.use(webpackHotMiddleware(compiler, {
   log: logger.info.bind(logger),
 }));
 
+const indexFileName = path.join(webpackClientConfig.output.path, 'index.html');
 if (HAS_PRERENDERING) {
   // WDM uses an in-memory file system, we need to
   // persist index.html to the real file system because it will be used by the pre-rendering server.
   compiler.plugin('done', () => {
     const memoryFs = wdmInstance.fileSystem;
 
-    const indexFileName = path.join(webpackClientConfig.output.path, 'index.html');
     memoryFs.readFile(indexFileName, (readError, file) => {
       if (readError) {
         fs.unlink(file);
@@ -77,6 +77,13 @@ if (HAS_PRERENDERING) {
 
   // redirect requests for non-static documents to the pre-rendering server.
   app.use(preRenderingHandler);
+
+} else {
+  // send index.html for all not found routes.
+  app.use((req, res) => {
+    const readStream = wdmInstance.fileSystem.createReadStream(indexFileName);
+    readStream.pipe(res);
+  });
 }
 
 app.listen(argv.port, () => {
