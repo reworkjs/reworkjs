2772f9db7f5292a8b407498f9c5ab43af85e8294
Guylian Cox <gc@pictawall.com>
2017-03-17 17:26:05 +0100

    env vars are strings.

--- src/internals/webpack/build-webpack-client.js
+++ src/internals/webpack/build-webpack-client.js
@@ -8,7 +8,7 @@ process.env.PROCESS_NAME = 'Server';
 
 logger.info('Building your client-side app, this might take a minute.');
 
-if (process.env.WATCH === true) {
+if (process.env.WATCH === 'true' || process.env.WATCH === true) {
   import('./build-webpack-client-watch');
 } else {
   compileWebpack(clientWebpackConfig, false, () => {
