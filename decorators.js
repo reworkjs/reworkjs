d3313bb5e3ac74b7a1962640736a041ec3afce4e
Guylian Cox <gc@pictawall.com>
2017-03-30 14:24:34 +0200

    Use an export syntax that webpack is able to understand

--- decorators.js
+++ decorators.js
@@ -1,3 +1,3 @@
-export container from './es/framework/common/decorators/container';
-export statelessComponent from './es/framework/common/decorators/statelessComponent';
+export { default as container } from './es/framework/common/decorators/container';
+export { default as statelessComponent } from './es/framework/common/decorators/statelessComponent';
 export * from './es/framework/common/decorators/provider';
