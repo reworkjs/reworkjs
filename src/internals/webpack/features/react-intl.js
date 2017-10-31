d893a2cb71576faa57df82475bcee14aabd39e3f
Guylian Cox <gc@pictawall.com>
2017-10-31 16:48:07 +0100

    feat: Add new feature that removes unused react-intl locales

---
+++ src/internals/webpack/features/react-intl.js
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import path from 'path';
+import webpack from 'webpack';
+import { uniq } from 'lodash';
+import BaseFeature from '../BaseFeature';
+import logger from '../../../shared/logger';
+import config from '../../../shared/framework-config';
+
+export default class ReactIntlFeature extends BaseFeature {
+
+  getFeatureName() {
+    return 'react-intl';
+  }
+
+  getDescription() {
+    return 'Removes locales of unsupported languages.';
+  }
+
+  isEnabled(enabled) {
+    if (!this.isProd()) {
+      return false;
+    }
+
+    return super.isEnabled(enabled);
+  }
+
+  visit(webpackConfig) {
+    const supportedLocales = uniq(fs.readdirSync(config.directories.translations)
+      .map(fileName => {
+        const locale = removeExtension(fileName);
+
+        return extractLanguage(locale);
+      }))
+      .filter(language => /^[a-z]+$/i.test(language));
+
+    logger.debug(`Removing react-intl locales unsupported by app language: ${supportedLocales.join(', ')}`);
+
+    const ignoreRegex = new RegExp(`\\.\\/(?!(?:${supportedLocales.join('|')})(?:[-_].+)?)[a-z]+\\.js`, 'i');
+
+    webpackConfig.injectPlugins(new webpack.IgnorePlugin(ignoreRegex, /react-intl\/locale-data$/));
+  }
+}
+
+function removeExtension(fileName: string): string {
+  const ext = path.extname(fileName);
+
+  return fileName.substring(0, fileName.length - ext.length);
+}
+
+function extractLanguage(locale: string): string {
+  const parts = locale.split(/[_-]/);
+  return parts[0];
+}
