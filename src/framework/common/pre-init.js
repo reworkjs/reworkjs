931601609d988560ca26d2d7c67693e6e1bf3a71
Guylian Cox <gc@pictawall.com>
2017-11-03 11:20:05 +0100

    fix: Don't use Logger before polyfills have been loaded

--- src/framework/common/pre-init.js
+++ src/framework/common/pre-init.js
@@ -1,21 +1,14 @@
 import loadPolyfills from '../common/load-polyfills';
 import { getDefault } from '../../shared/util/ModuleUtil';
-import logger from '../../shared/logger';
 
 export default async function loadPreInit() {
   await loadPolyfills();
 
   // webpack
   const preInit = getDefault(require('@@pre-init')); // eslint-disable-line
-  if (!preInit) {
-    logger.debug('No pre-init file.');
+  if (typeof preInit === 'function') {
+    await preInit();
   } else {
-    if (typeof preInit === 'function') {
-      await preInit();
-    } else {
-      await preInit;
-    }
-
-    logger.debug('pre-init loaded.');
+    await preInit;
   }
 }
