c7a224839d25cbef5b2ffe63afbb11dc3207aec9
Guylian Cox <gc@pictawall.com>
2017-11-24 14:33:53 +0100

    refactor: Merge pre-init & load-polyfills

--- src/framework/common/pre-init.js
+++ src/framework/common/pre-init.js
@@ -1,8 +1,10 @@
-import loadPolyfills from '../common/load-polyfills';
+import global from 'global';
+import '../../shared/regenerator';
+import './source-map-support';
 import { getDefault } from '../../shared/util/ModuleUtil';
 
 export default async function loadPreInit() {
-  await loadPolyfills();
+  await loadReworkPolyfills();
 
   // webpack
   const preInit = getDefault(require('@@pre-init')); // eslint-disable-line
@@ -11,4 +13,26 @@ export default async function loadPreInit() {
   } else {
     await preInit;
   }
+}
+
+/**
+ * Load the list of polyfills used by reworkjs
+ */
+function loadReworkPolyfills() {
+  const promises = [];
+
+  if (!global.Intl) {
+    promises.push(
+      import('intl'),
+
+      // TODO dynamically load appropriate intl locales
+      import('intl/locale-data/jsonp/en.js'),
+    );
+  }
+
+  if (typeof Symbol === 'undefined') {
+    promises.push(import('core-js/modules/es6.symbol'));
+  }
+
+  return Promise.all(promises);
 }
