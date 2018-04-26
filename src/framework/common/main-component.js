fc2c95e4d8f23d3bfba9e2808d5929ac72f491f6
Guylian Cox <gc@madkings.com>
2018-04-26 18:48:40 +0200

    fix: only import framework-config in bundle in development mode

--- src/framework/common/main-component.js
+++ src/framework/common/main-component.js
@@ -1,12 +1,14 @@
-import frameworkConfig from '../../shared/framework-config';
 import { getDefault } from '../../shared/util/ModuleUtil';
 import { isReactComponent } from '../util/ReactUtil';
 
 // WEBPACK
-const component = getDefault(require('@@main-component')); // eslint-disable-line
+const component = getDefault(require('@@main-component'));
 
-if (!isReactComponent(component)) {
-  throw new TypeError(`React entry point (${frameworkConfig['entry-react']}) is not a react component.`);
+if (process.env.NODE_ENV === 'development') {
+  if (!isReactComponent(component)) {
+    const frameworkConfig = require('../../shared/framework-config');
+    throw new TypeError(`React entry point (${frameworkConfig['entry-react']}) is not a react component.`);
+  }
 }
 
 export default component;
