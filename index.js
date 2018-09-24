05d241bfa4f161a7e7022cc690d30b3bf54be3d0
Guylian Cox <gc@madkings.com>
2018-09-24 13:53:12 +0200

    feat: move public modules from index.js to individual sub-modules
    first part of https://github.com/reworkjs/reworkjs/issues/43

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-export { default as logger } from './es/shared/logger';
 export { default as LanguageProvider } from './es/framework/app/providers/LanguageProvider';
 export { store } from './es/framework/common/kernel';
-export { default as debug } from './es/framework/common/debug';
 export { throttle } from './es/framework/common/decorators/provider';
-export { default as argv } from './es/framework/common/app-argv';
