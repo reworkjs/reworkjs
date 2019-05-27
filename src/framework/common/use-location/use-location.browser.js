40b23c81d87d126a72b984c3948fa33835b48003
Guylian Cox <gc@madkings.com>
2019-05-27 18:02:13 +0200

    feat: make use-location return URL & cause re-render on change

--- src/framework/common/use-location/use-location.browser.js
+++ src/framework/common/use-location/use-location.browser.js
@@ -1,5 +1,23 @@
 // @flow
 
-export function useLocation(): string {
-  return location.href;
+import { useRef } from 'react';
+import useRouter from 'use-react-router';
+
+export function useLocation(): URL {
+  // we use React Router so useLocation causes a re-render if location changes in browser.
+  const router = useRouter();
+
+  const url = new URL(window.location.href); // get origin from `location`
+  url.pathname = router.pathname;
+  url.search = router.search;
+  url.hash = router.hash;
+
+  const urlObjectRef = useRef(url);
+
+  // only return a new object if href changed, or object was tampered with.
+  if (urlObjectRef.current.href !== url.href) {
+    urlObjectRef.current = url;
+  }
+
+  return urlObjectRef.current;
 }
