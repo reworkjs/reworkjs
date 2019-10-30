eb062c0de59ed2395549548d0b99b8b7de57c3b6
Guylian Cox <gc@madkings.com>
2019-10-30 10:30:28 +0100

    build(core): rename to @reworkjs/core

---
+++ scripts/publish.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+PROPER_NAME=$(./node_modules/.bin/json -f package.json name)
+
+echo "Publishing @reworkjs/core"
+
+npm publish --access public
+
+echo "Publishing old alias @reworkjs/reworkjs"
+
+./node_modules/.bin/json -I -f package.json -e "this.name='@reworkjs/reworkjs'"
+
+npm publish --access public
+
+./node_modules/.bin/json -I -f package.json -e "this.name='$PROPER_NAME'"
