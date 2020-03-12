9eae42caa17be9443eb4628f72e337a5e4bfc597
Guylian Cox <hello@guylian.me>
2020-03-12 15:05:45 +0100

    build: update minimum browser support. Drop safari < 11

--- src/internals/babel/internal-babel-preset.js
+++ src/internals/babel/internal-babel-preset.js
@@ -10,14 +10,24 @@ module.exports = function buildPreset(api, opts = {}) {
 
   const { buildEsModules, ...passDownOpts } = opts;
 
+  const frameworkTargets = [
+    'chrome >= 72', // january 2019
+    'safari >= 11', // sept. 2018
+    'iOS >= 11',
+    'firefox >= 64', // january 2019
+    'maintained node versions',
+  ];
+
   const preset = buildGlobalPreset(api, {
     ...passDownOpts,
 
     '@babel/preset-env': buildEsModules ? {
       ...passDownOpts['@babel/preset-env'],
-      targets: 'maintained node versions',
+      targets: frameworkTargets,
       modules: 'commonjs',
-    } : {},
+    } : {
+      targets: frameworkTargets,
+    },
 
     '@babel/plugin-transform-runtime': {
       useESModules: !buildEsModules,
