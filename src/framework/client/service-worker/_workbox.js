39433c9f44f499501d35fa28400200ddacf3c512
Guylian Cox <hello@guylian.me>
2021-01-05 17:44:38 +0100

    feat: disable workbox cache in development

---
+++ src/framework/client/service-worker/_workbox.js
@@ -0,0 +1,39 @@
+// @flow
+
+import { enable as enableNavigationPreload } from 'workbox-navigation-preload/enable';
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+import { NavigationRoute } from 'workbox-routing/NavigationRoute';
+import { registerRoute } from 'workbox-routing/registerRoute';
+import { NetworkOnly } from 'workbox-strategies/NetworkOnly';
+
+// download assets ahead of time & create routes to them.
+precacheAndRoute(self.__WB_MANIFEST);
+
+const CACHE_NAME = 'offline-html';
+const FALLBACK_HTML_URL = '/index.html';
+// Populate the cache with the offline HTML page.
+self.addEventListener('install', async event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.add(FALLBACK_HTML_URL)),
+  );
+});
+
+enableNavigationPreload();
+
+const networkOnly = new NetworkOnly();
+const navigationHandler = async params => {
+  try {
+    // Attempt a network request.
+    return await networkOnly.handle(params);
+  } catch (error) {
+    // If it fails, return the cached HTML.
+    return caches.match(FALLBACK_HTML_URL, {
+      cacheName: CACHE_NAME,
+    });
+  }
+};
+
+// Register this strategy to handle all navigations.
+registerRoute(
+  new NavigationRoute(navigationHandler),
+);
--- src/framework/client/service-worker/index.js
+++ src/framework/client/service-worker/index.js
@@ -1,46 +1,8 @@
 // @flow
 
-import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-import { enable as enableNavigationPreload } from 'workbox-navigation-preload/enable';
-import { NavigationRoute } from 'workbox-routing/NavigationRoute';
-import { registerRoute } from 'workbox-routing/registerRoute';
-import { NetworkOnly } from 'workbox-strategies/NetworkOnly';
-
 // import app service worker
 // eslint-disable-next-line import/no-unresolved
 import 'val-loader!./_get-app-sw';
-
-// download assets ahead of time & create routes to them.
-precacheAndRoute(self.__WB_MANIFEST);
-
-const CACHE_NAME = 'offline-html';
-const FALLBACK_HTML_URL = '/index.html';
-// Populate the cache with the offline HTML page.
-self.addEventListener('install', async event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.add(FALLBACK_HTML_URL)),
-  );
-});
-
-enableNavigationPreload();
-
-const networkOnly = new NetworkOnly();
-const navigationHandler = async params => {
-  try {
-    // Attempt a network request.
-    return await networkOnly.handle(params);
-  } catch (error) {
-    // If it fails, return the cached HTML.
-    return caches.match(FALLBACK_HTML_URL, {
-      cacheName: CACHE_NAME,
-    });
-  }
-};
-
-// Register this strategy to handle all navigations.
-registerRoute(
-  new NavigationRoute(navigationHandler),
-);
 
 // allow update UX to install the new SW.
 self.addEventListener('message', event => {
@@ -48,3 +10,20 @@ self.addEventListener('message', event => {
     self.skipWaiting();
   }
 });
+
+/**
+ * HACK: "InjectManifest" insists that "self. __WB_MANIFEST" must be mentioned *once* in the code,
+ * But we don't use Workbox caching in Watch mode as its manifest is broken.
+ *
+ * So here we go:
+ *
+ * self.__WB_MANIFEST
+ *
+ * There
+ *
+ * See {@link ServiceWorkerFeature#visit} for more details
+ */
+
+if (process.env.NODE_ENV === 'production') {
+  require('./_workbox');
+}
