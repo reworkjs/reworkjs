92bbf16972e57fe3e86601e62dbdeba63f716570
Guylian Cox <hello@guylian.me>
2021-12-22 13:08:11 +0100

    refactor: cleanup

--- src/internals/babel/global-babel-preset.js
+++ src/internals/babel/global-babel-preset.js
@@ -5,16 +5,12 @@
  * - stable ES features
  * - optimisations
  */
-import PresetEnv from '@babel/preset-env';
-import PluginRuntime from '@babel/plugin-transform-runtime';
-import PluginLodash from 'babel-plugin-lodash';
-import PluginTransformReactConstantElements from '@babel/plugin-transform-react-constant-elements';
 
 export default function buildPreset(api, opts = {}) {
 
   const preset = {
     presets: [
-      [PresetEnv.default, {
+      ['@babel/preset-env', {
         modules: false,
         loose: true,
         ...opts['@babel/preset-env'],
@@ -23,7 +19,7 @@ export default function buildPreset(api, opts = {}) {
 
     plugins: [
       '@loadable/babel-plugin',
-      [PluginRuntime.default, {
+      ['@babel/plugin-transform-runtime', {
         corejs: false,
         helpers: true,
         regenerator: true,
@@ -35,8 +31,8 @@ export default function buildPreset(api, opts = {}) {
   const env = process.env.BABEL_ENV || process.env.NODE_ENV;
   if (env === 'production') {
     preset.plugins.push(
-      PluginLodash,
-      PluginTransformReactConstantElements.default,
+      'babel-plugin-lodash',
+      '@babel/plugin-transform-react-constant-elements',
     );
   }
 
